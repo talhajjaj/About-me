c18a019e516eb33604d7f20cc8fc05a146c64537
talhajjaj <taima.hajaj@gmail.com>
2021-04-27 16:42:48 +0300

    add

--- js/app.js
+++ js/app.js
@@ -3,42 +3,41 @@
 alert('welcome to our page')
 console.log("welcome to our page")
 
-let userName=prompt('enter your name')
+let userName = prompt('enter your name')
 {
- console.log('Hello' + userName);}
- alert ('hello'+ userName)
+    console.log('Hello' + userName);
+}
+alert('hello' + userName)
 
-let love= prompt('do you love watching anime?')
-if (love='yes')
-{console.log('yay')}
-else if (love="no")
-{console.log("why")}
+let love = prompt('do you love watching anime?')
+if (love = 'yes') { console.log('yay') }
+else if (love = "no") { console.log("why") }
 
-let feel=prompt('are you happy today?')
-if (feel="yes"){
+let feel = prompt('are you happy today?')
+if (feel = "yes") {
     console.log('cool')
 
 }
 
-else if (feel="no"){
+else if (feel = "no") {
     console.log('dont be')
 }
 
 
- let answer= prompt('enter how many anime you have watched?');
+let answer = prompt('enter how many anime you have watched?');
 
- if (answer >= 2) {
+if (answer >= 2) {
 
-     console.log('true');
- } else {
+    console.log('true');
+} else {
 
     console.log('false');
- }
+}
 
- let hours= prompt('enter how many hours do you work in week?');
- if (hours>=50){
-console.log("false")
- }
+let hours = prompt('enter how many hours do you work in week?');
+if (hours >= 50) {
+    console.log("false")
+}
 else {
     console.log('True')
 }
@@ -72,3 +71,60 @@ switch ('drikstype') {
 }
 
 
+// let age = prompt('what do you think how old am i ?');
+// while (age <22  || age >22 ) {
+//    alert ('too high');
+
+//   console.log('too high');
+// }
+//   if (age =22) {
+//     alert('great job')
+//       console.log ('great job')
+//   }
+//   else if (age <0 ){
+//       alert ('too low')
+//       console.log('too low')
+//   }
+
+
+
+
+
+
+for (let i = 0; i < 4; i++) {
+    let age = prompt('what do you think how old am i ?');
+    if (age < 22 || age > 22) {
+
+        alert('too high');
+
+        console.log('too high');
+    }
+    else if (age = 22) {
+        alert('great job')
+        console.log('great job')
+
+        break;
+    }
+
+    else if (age < 0) {
+        alert('too low')
+        console.log('too low')
+    }
+
+}
+
+for(let i =0;i<6;i++)
+let colors = ['Red', 'yellow', 'green', 'blue', 'purple', 'pink'];
+let useranswer = prompt('what is my fav rainbow color?');
+let hint = false;
+for (let i = 0; i < colors.length; i++) {
+    if (userAnswer === colors[i]) {
+        console.log('yes it is');
+        hint = true;
+        break;
+    }
+}
+if (hint=== false) {
+    alert ('wrong')
+    console.log('wrong');
+}
