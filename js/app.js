34c57cf936c886e48433eba0ec2c9c435b179435
talhajjaj <taima.hajaj@gmail.com>
2021-04-28 19:52:49 +0300

    edit alerts

--- js/app.js
+++ js/app.js
@@ -7,66 +7,88 @@ let userName = prompt('enter your name')
 {
     console.log('Hello' + userName);
 }
-alert('hello' + userName)
+alert('hello ' + userName + ' lets play a guessing game')
+ 
+let score =0;
 
-let love = prompt('do you love watching anime?')
-if (love = 'yes')
-alert('correct')
-
-else if (love = "no") 
-alert('wrong')
-
-let feel = prompt('are you happy today?')
-if (feel = "yes") 
-alert('cool')
-
-else if (feel = "no") 
-alert('dont be')
+let lovewatching = prompt('do you think i love watching anime?')
+if (lovewatching == 'yes'|| lovewatching == 'y' ){
+// console.log('correct');
+alert('correct');
+}
+else if (lovewatching == "no"||"n") {
+    // console.log('wrong');
+    alert('wrong')
+    score++;
+}
 
 
-let answer = prompt('enter how many anime you have watched?');
+// let feel = prompt('are you happy today?')
+// if (feel = "yes") 
+// alert('cool')
 
-if (answer >= 2) {
+// else if (feel = "no") 
+// alert('dont be')
 
-    console.log('true');
+
+let answer = prompt('enter how many anime do you think i have watched?');
+
+if (answer >= 5) {
+
+    // console.log('correct');
+    alert('correct');
+    score++
 } else {
 
-    console.log('false');
+    // console.log('wrong');
+    alert('wrong')
 }
 
-let hours = prompt('enter how many hours do you work in week?');
-if (hours >= 50) {
-    console.log("false")
+let studyhours = prompt('how many hours do you think i study in week?');
+if (studyhours >= 50) {
+    // console.log("correct")
+    alert('correct')
+    score++
 }
 else {
-    console.log('True')
+    // console.log('wrong')
+    alert('wrong')
 }
 
-let animetype = prompt('do you like action anime?').toUpperCase();
+let animetype = prompt('do you think that i like action anime?').toUpperCase();
 
 switch ('animeType') {
     case 'YES':
-        console.log('true');
+        // console.log('correct');
+        alert('correct')
+        score++
         break;
     case 'NO':
-        console.log('false');
+        // console.log('wrong');
+        alert('wrong')
         break;
     default:
-        console.log('wrong answer');
+        // console.log('wrong answer');
+        alert('wrong answer')
         break;
 }
 
-let drinkstype = prompt('do you like cold or hot drinks?').toLocaleLowerCase();
+let drinkstype = prompt('what do you think i like to drink cold or hot drinks?').toLocaleLowerCase();
 
 switch ('drikstype') {
     case 'hot':
-        console.log('yummy');
+        // console.log('yummy');
+        alert('correct')
+        score++
+
         break;
     case 'cold':
-        console.log('awch');
+        // console.log('awch');
+        alert('wrong')
         break;
     default:
-        console.log('wrong answer');
+        // console.log('wrong answer');
+        alert('wrong answer')
         break;
 }
 
@@ -91,57 +113,52 @@ switch ('drikstype') {
 
 
 
-for (let i = 0; i < 4; i++) {
-    let age = prompt('what do you think how old am i ?');
-    if (age < 22 || age > 22) {
-
+for (let i = 0; i < 4; i++){
+    let myage = prompt('what do you think how old am i ?');
+    if (myage > 22) {
         alert('too high');
 
-        console.log('too high');
+        // console.log('too high');
     }
     else if (age = 22) {
-        alert('great job')
-        console.log('great job')
+        alert('correct')
 
+        // console.log('correct')
+          score++
         break;
     }
 
-    else if (age < 0) {
+    else if (age < 22) {
+      
         alert('too low')
-        console.log('too low')
+        // console.log('too low')
+        
     }
 
 }
 
-// for(let i =0;i<6;i++)
-// let colors = [];
-// let useranswer = prompt('what is my fav rainbow color?');
-// let hint = false;
-// for (let i = 0; i < colors.length; i++) {
-//     if (userAnswer === colors[i]) {
-//         console.log('yes it is');
-//         hint = true;
-//         break;
-//     }
-// }
-// if (hint=== false) {
-//     alert ('wrong')
-//     console.log('wrong');
-// }
 
-let rainbowcolor = ['red', 'yellow', 'green', 'blue', 'purple', 'pink'];
+
+let favfruit = ['cherry', 'apple', 'strawberry', 'oranges'];{
 let hint=false ;
 for(let attempt=1; attempt <=6;attempt++){
-   let userAnswer = prompt('what is my fav rainbow color?');
-for (let i = 0; i < 6; i++) {
-    if (userAnswer === rainbowcolor[i]) {
+let userAnswer = prompt('what is my fav fruit ?');
+for (let i = 0; i < favfruit.length; i++) {
+    if (userAnswer === favfruit[i]) {
+
         alert('yes it is right ');
+         // console.log('yes it is right');
         hint= true;
+        score++;
         break;
     }
 }
-if (hint === true) {
-    break;
+ if (hint === false) {
+    // console.log('wrong');
+    alert('wrong');
 }
 }
-alert('the right answer is red, yellow, green,blue, purple, pink')
+}
+alert('the right answer is cherry apple strawberry oranges') ;
+alert('your score is '+ score + ' of 7');
+alert('Thank you for your time ');
